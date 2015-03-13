6655c2be51ebbfcd10196b54a66c5f5dbdefd908
Chuck Pierce <chuckmpierce@gmail.com>
2015-03-13 13:27:01 -0400

    completed responsive quote directive

--- client/app/directives/quote/quote.directive.js
+++ client/app/directives/quote/quote.directive.js
@@ -4,8 +4,23 @@ angular.module('percolateApp')
   .directive('quote', function () {
     return {
       templateUrl: 'app/directives/quote/quote.html',
-      restrict: 'EA',
+      restrict: 'E',
+      transclude: true,
+      scope: {
+      	person: '@',
+      	image: '@',
+      	qtype: '@'
+      },
       link: function (scope, element, attrs) {
+      	var pad = element.children().children();
+      	if(attrs.qtype === 'highlighted') {
+      		console.log(true);
+      		element.addClass('quote-highlighted');
+      		pad.addClass('pad-highlighted');
+      	} else {
+      		element.addClass('quote-normal');
+      		pad.addClass('pad-normal');
+      	}
       }
     };
   });
